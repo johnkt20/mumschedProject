612f368b122f9f9cf2fa9d1f8657a2407caf647e
faafadis <42832446+faafadis@users.noreply.github.com>
2021-03-08 20:11:42 -0600

    student form update

--- Mum-Schedule/src/main/java/edu/mum/mumsched/domain/Student.java
+++ Mum-Schedule/src/main/java/edu/mum/mumsched/domain/Student.java
@@ -6,14 +6,43 @@ import java.util.List;
 
 @Entity
 public class Student {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    @ManyToOne
-    private Entry entry;
-    @OneToMany
-    private List<RegisterRecorde> registerRecordes=new ArrayList<>();
-// new student
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long Id;
+    private String firstName;
+    private String lastName;
 
+    public Long getId() {
+        return Id;
+    }
 
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
